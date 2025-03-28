e2b50caeedeecec599ea51e637172f2406b67d05
sp1deydev <duckhanhthientran@gmail.com>
2025-03-28 14:38:50 +0700

    initial commit

---
+++ src/main/java/com/payment/vnpay/config/VNPayConfig.java
@@ -0,0 +1,4 @@
+package com.payment.vnpay.config;
+
+public class VNPayConfig {
+}
---
+++ src/test/java/com/payment/vnpay/VnpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.vnpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VnpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
